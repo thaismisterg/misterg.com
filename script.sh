4e870f2eb31a46b07e8aeb21139902930d5fc45d
thaismisterg <30526397+thaismisterg@users.noreply.github.com>
2017-07-28 08:49:24 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 c=1
 while [ $c -le 60 ]
 do
- wget -O - http://m-l-a-b-s.com/refresh.php >/dev/null 2>&1
+ wget -O - http://m-i-s-t-e-r-g.com/refresh.php >/dev/null 2>&1
  sleep 1
  (( c++ ))
 done
